69010e197810a3f6534b372f6fed43cd864445f4
Lems0n <abdullasaidahmedov1@gmail.com>
2024-12-30 12:07:28 +0300

    file engine.py was updated

--- app/database/core/engine.py
+++ app/database/core/engine.py
@@ -3,7 +3,6 @@ from secure import settings
 
 
 engine = create_async_engine(
-    "sqlite+aiosqlite:///test.db"
-    # settings.DATABASE.postgres_connection(),
+    settings.database.postgres_connection(),
 )
 
